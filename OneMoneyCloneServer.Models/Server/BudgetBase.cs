9df0dd4cdddf61f5b23f441007fdf53d79500c5e
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-20 21:52:00 +0200

    Update BudgetBase.cs

--- OneMoneyCloneServer.Models/Server/BudgetBase.cs
+++ OneMoneyCloneServer.Models/Server/BudgetBase.cs
@@ -6,6 +6,6 @@ public abstract class BudgetBase : CreationTrackingEntity
 	public Guid UserId { get; set; }
 	public User User { get; set; } = default!;
 	public decimal Amount { get; set; }
-	public DateTime StartDate { get; set; }
-	public DateTime EndDate { get; set; }
+	public DateOnly StartDate { get; set; }
+	public DateOnly EndDate { get; set; }
 }
