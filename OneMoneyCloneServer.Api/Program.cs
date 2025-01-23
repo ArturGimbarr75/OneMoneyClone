6ab568ff42c5fe8c721002be91924df9f36bcdbd
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-23 19:50:21 +0200

    Update Program.cs

--- OneMoneyCloneServer.Api/Program.cs
+++ OneMoneyCloneServer.Api/Program.cs
@@ -1,23 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Swashbuckle.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+string connectionString = builder.Configuration.GetConnectionString("DebugConnection")!;
+OneMoneyCloneServer.Persistence.Registrator.RegisterDatabase(builder.Services, connectionString);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+	});
+
+	app.MapGet("/", () => Results.Redirect("/swagger"));
+	app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
