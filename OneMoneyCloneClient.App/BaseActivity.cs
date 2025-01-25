45af607e142b75de7bc758216f39c817f254e2c8
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-25 18:06:05 +0200

    Add accounts layout

---
+++ OneMoneyCloneClient.App/BaseActivity.cs
@@ -0,0 +1,12 @@
+﻿using Android.Content.PM;
+
+namespace OneMoneyCloneClient.App;
+
+public class BaseActivity : Activity
+{
+	protected override void OnCreate(Bundle? savedInstanceState)
+	{
+		base.OnCreate(savedInstanceState);
+		RequestedOrientation = ScreenOrientation.Portrait;
+	}
+}
--- OneMoneyCloneClient.App/MainActivity.cs
+++ OneMoneyCloneClient.App/MainActivity.cs
@@ -1,13 +1,24 @@
+using Android;
+using Android.Content.PM;
+
 namespace OneMoneyCloneClient.App;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
-public class MainActivity : Activity
+public class MainActivity : BaseActivity
 {
 	protected override void OnCreate(Bundle? savedInstanceState)
 	{
 		base.OnCreate(savedInstanceState);
+		RequestedOrientation = ScreenOrientation.Portrait;
 
-		// Set our view from the "main" layout resource
-		SetContentView(Resource.Layout.activity_main);
+		int layoutId = GetDefaultLayoutId();
+		SetContentView(layoutId);
+		ActionBar?.Hide();
+	}
+
+	private int GetDefaultLayoutId()
+	{
+		// TODO: Implement this method, make oportunity to select default layout
+		return Resource.Layout.accounts;
 	}
 }
