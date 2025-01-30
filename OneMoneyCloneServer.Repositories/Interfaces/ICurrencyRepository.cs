ccfdbe7d4b436f85f5adc27b9e80461fa4a7749c
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-30 23:28:17 +0200

    Create ICurrencyRepository.cs

---
+++ OneMoneyCloneServer.Repositories/Interfaces/ICurrencyRepository.cs
@@ -0,0 +1,9 @@
+﻿using OneMoneyCloneServer.Models.Server;
+
+namespace OneMoneyCloneServer.Repositories.Interfaces;
+
+public interface ICurrencyRepository
+{
+	Task<Currency?> GetCurrencyByIdAsync(Guid id);
+	Task<IEnumerable<Currency>> GetCurrenciesAsync();
+}
