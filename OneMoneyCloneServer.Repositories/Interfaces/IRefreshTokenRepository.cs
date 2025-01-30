c3ef0d8f3d022c5fb9fffe4a84012fa4e3e5cfb2
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-30 23:28:20 +0200

    Create IRefreshTokenRepository.cs

---
+++ OneMoneyCloneServer.Repositories/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using OneMoneyCloneServer.Models.Server;
+
+namespace OneMoneyCloneServer.Repositories.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+	Task<RefreshToken?> GetRefreshTokenByTokenAsync(string token);
+	Task<RefreshToken?> CreateRefreshTokenAsync(RefreshToken refreshToken);
+	Task<RefreshToken?> UpdateRefreshTokenAsync(RefreshToken refreshToken);
+	Task<RefreshToken?> DeleteRefreshTokenAsync(string token);
+}
