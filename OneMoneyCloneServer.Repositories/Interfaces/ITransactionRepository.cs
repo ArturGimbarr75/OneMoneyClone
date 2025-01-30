4cde079ac9ca24e346afdaf83091dfeb5a97401b
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-30 23:28:27 +0200

    Create ITransactionRepository.cs

---
+++ OneMoneyCloneServer.Repositories/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using OneMoneyCloneServer.Models.Server;
+
+namespace OneMoneyCloneServer.Repositories.Interfaces;
+
+public interface ITransactionRepository
+{
+	Task<Transaction?> GetTransactionByIdAsync(Guid id);
+	Task<Transaction?> CreateTransactionAsync(Transaction transaction);
+	Task<Transaction?> UpdateTransactionAsync(Transaction transaction);
+	Task<Transaction?> DeleteTransactionAsync(Guid id);
+	Task<IEnumerable<Transaction>> GetTransactionsByUserIdAsync(Guid userId);
+	Task<IEnumerable<Transaction>> GetTransactionsByUserIdAndPeriodAsync(Guid userId, DateOnly startDate, DateOnly endDate);
+	Task<IEnumerable<Transaction>> GetTransactionsByUserIdAndCategoryAsync(Guid userId, Guid categoryId);
+	Task<IEnumerable<Transaction>> GetTransactionsByUserIdAndCategoryAndPeriodAsync(Guid userId, Guid categoryId, DateOnly startDate, DateOnly endDate);
+}
