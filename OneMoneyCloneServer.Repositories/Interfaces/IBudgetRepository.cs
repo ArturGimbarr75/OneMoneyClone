b6164fcd713ff25ebaca14848797c4b968726b25
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-30 23:03:13 +0200

    Create IBudgetRepository.cs

---
+++ OneMoneyCloneServer.Repositories/Interfaces/IBudgetRepository.cs
@@ -0,0 +1,14 @@
+﻿using OneMoneyCloneServer.Models.Server;
+
+namespace OneMoneyCloneServer.Repositories.Interfaces;
+
+public interface IBudgetRepository
+{
+	Task<Budget?> GetBudgetByIdAsync(Guid id);
+	Task<Budget?> CreateBudgetAsync(Budget budget);
+	Task<Budget?> UpdateBudgetAsync(Budget budget);
+	Task<Budget?> DeleteBudgetAsync(Guid id);
+	Task<IEnumerable<Budget>> GetBudgetsByUserIdAsync(Guid userId);
+	Task<IEnumerable<Budget>> GetBudgetsByUserIdAndPeriodAsync(Guid userId, DateOnly startDate, DateOnly endDate);
+	Task<bool> IsBudgetForPeriodExists(Guid userId, Guid expenceBudgetId, DateOnly startDate, DateOnly endDate);
+}
