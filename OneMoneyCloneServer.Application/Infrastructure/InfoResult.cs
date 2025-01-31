fba5162f532f74542f848403196c787d53052436
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-01-31 02:17:59 +0200

    Add result types

---
+++ OneMoneyCloneServer.Application/Infrastructure/InfoResult.cs
@@ -0,0 +1,31 @@
+﻿namespace OneMoneyCloneServer.Application.Infrastructure;
+
+public class InfoResult<T, TError> : Result<T, TError> where TError : struct, Enum
+{
+	public IEnumerable<string> Info { get; set; } = default!;
+
+	public static implicit operator InfoResult<T, TError>(T res)
+	{
+		return new InfoResult<T, TError>
+		{
+			Value = res
+		};
+	}
+
+	public static implicit operator InfoResult<T, TError>(TError error)
+	{
+		return new InfoResult<T, TError>
+		{
+			Error = error
+		};
+	}
+
+	public static InfoResult<T, TError> WithInfo(TError error, params IEnumerable<string> info)
+	{
+		return new InfoResult<T, TError>
+		{
+			Error = error,
+			Info = info
+		};
+	}
+}
