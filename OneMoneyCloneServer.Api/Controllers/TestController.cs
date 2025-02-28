c681a3eeeb3677dacb0ee7d6a49881d39a5861a1
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2025-02-28 23:28:53 +0200

    Fix Identity core

--- OneMoneyCloneServer.Api/Controllers/TestController.cs
+++ OneMoneyCloneServer.Api/Controllers/TestController.cs
@@ -1,11 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OneMoneyCloneServer.Persistence;
 
 namespace OneMoneyCloneServer.Api.Controllers
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
-    public class TestController : ControllerBase
+	[Route("api/[controller]")]
+	public class TestController : ControllerBase
     {
         private readonly ILogger<TestController> _logger;
 
--- OneMoneyCloneServer.Api/Registrator.cs
+++ OneMoneyCloneServer.Api/Registrator.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OneMoneyCloneServer.Models.Server;
 using OneMoneyCloneServer.Persistence;
+using System.Text;
 
 namespace OneMoneyCloneServer.Api;
 
@@ -8,7 +13,7 @@ internal static class Registrator
 {
 	public static void AddIdentity(this IServiceCollection services)
 	{
-		services.AddIdentity<User, IdentityRole<Guid>>()
+		services.AddIdentityCore<User>()
 				.AddEntityFrameworkStores<ApplicationDbContext>()
 				.AddDefaultTokenProviders();
 	}
@@ -16,12 +21,61 @@ internal static class Registrator
 	public static void AddSwagger(this WebApplication app)
 	{
 		app.UseSwagger();
-		app.UseSwaggerUI(c =>
+		app.UseSwaggerUI(options =>
 		{
-			c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+			options.RoutePrefix = string.Empty;
+			options.OAuthClientId("swagger-ui");
+			options.OAuthAppName("Swagger UI");
 		});
+	}
 
-		app.MapGet("/", () => Results.Redirect("/swagger"));
-		app.UseDeveloperExceptionPage();
+	public static void AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ValidIssuer = configuration["Jwt:Issuer"],
+						ValidAudience = configuration["Jwt:Audience"],
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+					};
+				});
+		services.AddAuthorization();
+	}
+
+	public static void AddSwagger(this IServiceCollection services)
+	{
+		services.AddSwaggerGen(options =>
+		{
+			options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+			{
+				In = ParameterLocation.Header,
+				Description = "Please insert JWT with Bearer into field",
+				Name = "Authorization",
+				Type = SecuritySchemeType.Http,
+				Scheme = "Bearer"
+			});
+
+			options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+			{
+				{
+					new OpenApiSecurityScheme()
+					{
+						Reference = new OpenApiReference()
+						{
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+						}
+					},
+					Array.Empty<string>()
+				}
+			});
+		});
 	}
 }
